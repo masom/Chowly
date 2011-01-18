b745e37e796ca547a6867627090d63a63679245b
Martin Samson <pyrolian@gmail.com>
2011-01-18 18:00:06 -0500

    Added tickets

---
+++ views/tickets/received.html.php
@@ -0,0 +1,7 @@
+<h1>Thank you for contacting Chowly</h1>
+
+<p>We have received your message and will contact you shortly.</p>
+
+<p>Chowly greatly valuates customer feedback.</p>
+<p>Every message will be read and answered personnaly.</p>
+<p>~Chowly Team</p>
