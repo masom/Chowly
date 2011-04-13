28d893e3e5ac8114942e7e855b7af78f6eb41823
Martin Samson <pyrolian@gmail.com>
2011-04-13 13:22:06 -0400

    Fixes error rendering

--- views/errors/404.html.php
+++ views/errors/404.html.php
@@ -1,5 +1,10 @@
-<div id="content-panel">
-<h1>Well... 404</h1>
-<p>It seems the page you are looking for does not exists...</p>
-<p>Appropriate professionals have been notified.</p>
+<div id="ribbon">
+	<span>404 - Page Not Found</span>
+</div>
+<div id="content-wrapper">
+<div style="margin-left: auto;margin-right:auto; width: 500px; margin-top: 100px;">
+<h1>404</h1>
+<br />
+<p>It appears the page you requested is not available at this time.</p>
+</div>
 </div>
--- views/errors/500.html.php
+++ views/errors/500.html.php
@@ -1,4 +1,11 @@
-<h1>Mmmm... Something broke</h1>
-
-<p>Something died over here...</p>
-<p>Emergency server healthcare professionnals have been notified.</p>
+<div id="ribbon">
+	<span>500 - Chowly Internal Bleeding Error</span>
+</div>
+<div id="content-wrapper">
+<div style="margin-left: auto;margin-right:auto; width: 500px; margin-top: 100px;">
+<h1>500</h1>
+<br />
+<p>It seems we crashed and are terribly sorry.</p>
+<p>Our technical support team has been notified.</p>
+</div>
+</div>
