39d2ee48ea4ad2957e66483fa1bef68b7d2ba0af
Martin Samson <pyrolian@gmail.com>
2011-04-15 17:35:15 -0400

    Added a slug transform for element name

--- extensions/data/Model.php
+++ extensions/data/Model.php
@@ -13,5 +13,38 @@ class Model extends \lithium\data\Model{
 		$entity->updated = $date;
 		return parent::save($entity, $data, $options);
 	}
+	
+	/**
+     * Generate a unique slug
+     * 
+     * @params $options Array
+     *      - field: The field used to generate the slug
+     *      - separator: The optional separator symbol for spaces (default: -)
+     * @return String The unique pretty url.
+    */
+    public function slug($entity, Array $options = array()) {
+        $options += array('field'=>'name', 'separator' => '-');
+        
+        // all URLs are lowercase
+        $slug = date('y-m-d-') . strtolower($entity->{$options['field']});
+        $slug = \lithium\util\Inflector::slug($slug, $options['separator']);
+        
+        $conditions = array('slug' => $slug);
+        $conflicts = static::count(compact('conditions'));
+        
+        if($conflicts){
+	        $i = 0;
+	        $newSlug = '';
+	        while($conficts){
+	        	$i++;
+	        	$newSlug = "{$slug}-{$i}";
+        		$conditions = array('slug' => $newSlug);
+        		$conflicts = static::count(compact('conditions'));
+	        }
+	        // Out of conflict.
+	        $slug = $newSlug;
+        }
+        return $slug;
+    }
 }
 ?>
