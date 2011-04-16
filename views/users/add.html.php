b47e447e43f965393e9aa0c34dc85987dcb5ad9f
Martin Samson <pyrolian@gmail.com>
2011-04-16 15:19:35 -0400

    Fixes registration

--- views/users/add.html.php
+++ views/users/add.html.php
@@ -1,13 +1,17 @@
-<h1>Registration</h1>
-
-<?=$this->form->create($user,array('id'=>'UserRegistration'));?>
-<?=$this->form->field('name');?>
-<?=$this->form->field('email');?>
-<?=$this->form->field('password', array('id'=>'UserRegistrationPassword', 'type'=>'password'));?>
-<?=$this->form->field('password_repeat', array('id'=>'UserRegistrationPasswordRepeat', 'type'=>'password'));?>
-<button onclick="return false;" id="UserRegistrationSubmit">Register</button>
-<?=$this->form->end();?>
-
+<div id="ribbon">
+	<span>Registration</span>
+</div>
+<div id="content-wrapper">
+	<div style="margin:20px;">
+		<?=$this->form->create($user,array('id'=>'UserRegistration'));?>
+		<?=$this->form->field('name');?>
+		<?=$this->form->field('email');?>
+		<?=$this->form->field('password', array('id'=>'UserRegistrationPassword', 'type'=>'password'));?>
+		<?=$this->form->field('password_repeat', array('id'=>'UserRegistrationPasswordRepeat', 'type'=>'password'));?>
+		<button onclick="return false;" id="UserRegistrationSubmit">Register</button>
+		<?=$this->form->end();?>
+	</div>
+</div>
 <script type="text/javascript">
 
 $('#UserRegistrationSubmit').bind('click', function(){
