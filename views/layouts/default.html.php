70f20fab2dcfd292184a7c6e02262c08eb164010
Martin Samson <pyrolian@gmail.com>
2011-04-16 15:48:01 -0400

    Added boxed test

--- views/layouts/default.html.php
+++ views/layouts/default.html.php
@@ -39,24 +39,24 @@
 			<div style="float: left; height: 200px; width: 281px;">
 				<div style="background-image: url(/img/bottomboxtop.png); width: 281px; height: 16px;"></div>
 				<div style="background-color: #ffffff; width: 281px;">
-					<h1>What is Chowly?</h1>
-					<p>Lorem ipsum</p>
+					<h1>The Chowly Guarantee</h1>
+ 					<p>We are only as good as our last customer's experience. If you ever feel like Chowly messed up, email us a and we'll make it right – simple as that.</p>
 				</div>
 				<div style="background-image: url(/img/bottomboxbottom.png); width: 281px; height: 16px;"></div>
 			</div>
 			<div style="float: left; height: 200px; width: 281px; margin-left: 8px; margin-right: 8px;">
 				<div style="background-image: url(/img/bottomboxtop.png); width: 281px; height: 16px;"></div>
 				<div style="background-color: #ffffff; width: 281px;">
-					<h1>What is Chowly?</h1>
-					<p>Lorem ipsum</p>
+					<h1>Check out our blog</h1>
+					<p>We are looking to our early customers (that's you) to help shape Chowly and this is the perfect forum to provide your feedback.</p>
 				</div>
 				<div style="background-image: url(/img/bottomboxbottom.png); width: 281px; height: 16px;"></div>
 			</div>
 			<div style="float: left; height: 200px; width: 281px;">
 				<div style="background-image: url(/img/bottomboxtop.png); width: 281px; height: 16px;"></div>
 				<div style="background-color: #ffffff; width: 281px;">
-					<h1>What is Chowly?</h1>
-					<p>Lorem ipsum</p>
+					<h1>Feature Your Restaurant</h1>
+					<p>Want to be featured or learn more about Chowly.com? Send a email to restaurants@chowly.com and we will be happy to talk Chowly with you.</p>
 				</div>
 				<div style="background-image: url(/img/bottomboxbottom.png); width: 281px; height: 16px;"></div>
 			</div>
