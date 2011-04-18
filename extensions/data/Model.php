dac467bd499709258d998502ed59d4e0007f8144
Martin Samson <pyrolian@gmail.com>
2011-04-18 17:07:06 -0400

    QA

--- extensions/data/Model.php
+++ extensions/data/Model.php
@@ -11,40 +11,40 @@ class Model extends \lithium\data\Model{
 	protected $_meta = array('key' => '_id');
 	public function save($entity, $data = null, array $options = array()) {
 		$date = new \MongoDate(time());
-		
-		if(!$entity->exists()){
+
+		if (!$entity->exists()){
 			$entity->created = $date;
 		}
 
 		$entity->updated = $date;
 		return parent::save($entity, $data, $options);
 	}
-	
+
 	/**
      * Generate a unique slug
-     * 
-     * @params $options Array
+     * @param $entity lithium\data\Entity
+     * @param $options Array
      *      - field: The field used to generate the slug
      *      - separator: The optional separator symbol for spaces (default: -)
      * @return String The unique pretty url.
     */
-    public function slug($entity, Array $options = array()) {
+    public function slug($entity, array $options = array()){
         $options += array('field'=>'name', 'separator' => '-', 'prepend'=> '');
-        
+
         extract($options);
-        
+
         // all URLs are lowercase
-        $slug = $prepend.$separator.$entity->{$field};
-        $slug = date('Ymd'). $separator . strtolower($slug);
+        $slug = $prepend . $separator . $entity->{$field};
+        $slug = date('Ymd') . $separator . strtolower($slug);
         $slug = \lithium\util\Inflector::slug($slug, $separator);
-        
+
         $conditions = array('slug' => $slug);
         $conflicts = static::count(compact('conditions'));
-        
-        if($conflicts){
+
+        if ($conflicts){
 	        $i = 0;
 	        $newSlug = '';
-	        while($conflicts){
+	        while ($conflicts){
 	        	$i++;
 	        	$newSlug = "{$slug}-{$i}";
         		$conditions = array('slug' => $newSlug);
@@ -56,4 +56,5 @@ class Model extends \lithium\data\Model{
         return $slug;
     }
 }
+
 ?>
