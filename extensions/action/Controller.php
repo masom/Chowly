ae72c4812ec9de57eec8e4a6dd9e21d3e5494e0a
Martin Samson <pyrolian@gmail.com>
2011-04-18 17:33:41 -0400

    QA

--- extensions/action/Controller.php
+++ extensions/action/Controller.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Chowly Pick. Eat. Save!
  * @copyright	Copyright 2011, Martin Samson
@@ -7,18 +7,20 @@
 
 namespace chowly\extensions\action;
 
-use \lithium\net\http\Router;
-use \lithium\template\View;
-use \lithium\storage\Session;
+use lithium\net\http\Router;
+use lithium\template\View;
+use lithium\storage\Session;
 use chowly\models\Carts;
 
 class Controller extends \lithium\action\Controller{
-	
+
 	protected $Cart;
-	
+
 	protected function _init(){
 		parent::_init();
-		if($this->request->is('ssl') && !in_array($this->request->controller, array('checkouts','users'))){
+		$isSsl = $this->request->is('ssl');
+		$isSecured = in_array($this->request->controller, array('checkouts','users'));
+		if ( $isSsl && !$isSecured){
 			return $this->redirect(Router::match(
 				$this->request->params,
 				$this->request,
@@ -26,39 +28,40 @@ class Controller extends \lithium\action\Controller{
 				)
 			);
 		}
-		
+
 		$conditions = array('_id' => Session::read('cart.id'));
 		$this->Cart = Carts::first(compact('conditions'));
 
-		if(!$this->Cart){
+		if (!$this->Cart){
 			$this->Cart = Carts::create();
 			$this->Cart->_id = new \MongoId();
 			Session::write('cart.id', $this->Cart->_id);
 		}
-
 	}
+
 	/**
 	 * Generates an email test
 	 * @param array $data Information used by the templates
 	 * @param var $template Template to be used
-	 * @param var $controller Folder containing the template (based on controller name). Defaults to the current controller name
-	 * @throws \lithium\core\ConfigException
+	 * @param var $controller	Folder containing the template (based on controller name).
+	 * 							Defaults to the current controller name
+	 * @throws ConfigException
 	 * @return var The email text
 	 */
 	protected function _getEmail(array $data, $template = null, $controller = null){
-		
-		if(!$template){
+
+		if (!$template){
 			throw new \lithium\core\ConfigException("Undefined email template");
 		}
-		
+
 		$view  = new View(array(
 		    'paths' => array(
 		        'template' => '{:library}/views/{:controller}/{:template}.{:type}.php'
 		    )
 		));
-		
+
 		$controller = $controller ?: $this->request->params['controller'];
-		
+
 		return $view->render(
 		    'template',
 		    $data,
@@ -71,4 +74,5 @@ class Controller extends \lithium\action\Controller{
 		);
 	}
 }
+
 ?>
